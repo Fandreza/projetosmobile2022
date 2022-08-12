28d8aa38a4add5b2e4d3f7b2431076b31c652749
Fandreza <110498473+Fandreza@users.noreply.github.com>
2022-08-12 19:33:34 -0300

    Add files via upload

---
+++ 003-projeto/003-projeto.js
@@ -0,0 +1,7 @@
+var salario = 2000.00
+var aumento = 0.15
+var inflacao = 0.07
+novosalario = salario + (aumento * salario);
+novosalarioinflacao = novosalario - (aumento * salario );
+console.log (novosalario);
+console.log (novosalarioinflacao);
