da9ff0c4cdbb41172fe8c1007b00b73b86d42d98
Fandreza <110498473+Fandreza@users.noreply.github.com>
2022-08-16 20:30:02 -0300

    Add files via upload

---
+++ 006-projeto/app.js
@@ -0,0 +1,7 @@
+var num1 = 10;
+var num2 = 15;
+var num3 = 5;
+console.log(num1 > num2);
+if (num1 > num2) { 
+console.log( 'O numero ${num1}é maior que o ${num2}')}
+else {console.log(´ o numero ${num1} é menor que o numero${num2})}
